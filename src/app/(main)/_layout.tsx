24770e9967e175414c69af07c9fcf01bf92b0498
Miracle Friday <miraclef60@gmail.com>
2024-03-03 04:40:22 +0100

    fix: fixed main _layout.tsx

--- src/app/(main)/_layout.tsx
+++ src/app/(main)/_layout.tsx
@@ -1,14 +1,15 @@
 import { Stack } from "expo-router";
-
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function MainLayout() {
-
-
   return (
-
-    <Stack screenOptions={{}}>
-      <Stack.Screen options={{ headerShown: false }} />
-    </Stack>
-
+    <SafeAreaProvider>
+      <GestureHandlerRootView>
+        <Stack screenOptions={{}}>
+          <Stack.Screen options={{ headerShown: false }} />
+        </Stack>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
