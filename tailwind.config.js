4c5a843e9854e5910939bd17f6be3ce57e4869e6
Miracle Friday <miraclef60@gmail.com>
2024-03-13 01:51:24 +0100

    refactor: removed font asset folder and font config from app.config.ts and tailwindcss.config.js
    - Also removed the expo-font package

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {
-      fontFamily: {
-        NunitoSans: ["./assets/fonts/Nunito_Sans/NunitoSans-VariableFont_YTLC,opsz,wdth,wght.ttf", "./assets/fonts/Nunito_Sans/NunitoSans-Italic-VariableFont_YTLC,opsz,wdth,wght.ttf"],
-        PlusJakartaSans: ["./assets/fonts/Plus_Jakarta_Sans/PlusJakartaSans-VariableFont_wght.ttf", "./assets/fonts/Plus_Jakarta_Sans/PlusJakartaSans-Italic-VariableFont_wght.ttf"]
-      },
-    },
+    extend: {},
   },
   future: {
     hoverOnlyWhenSupported: true,
